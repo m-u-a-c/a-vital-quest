3dcb0cf693417d24f2fa312f5adeb2a94c40e8cd
MightyKoala <leonpayao@gmail.com>
2014-11-13 10:36:23 +0100

    Menu update

--- New Unity Project/Assets/Menus/MainMenu.cs
+++ New Unity Project/Assets/Menus/MainMenu.cs
@@ -6,11 +6,17 @@ public class MainMenu : MonoBehaviour
 	public Texture btnTexture;
 	void OnGUI ()
 	{
-		GUI.Box(new Rect(100,100,100,90), "A Vital Quest");
+		GUI.Box(new Rect(Screen.width/2,Screen.height/4,100,90), "A Vital Quest");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(125,150,100,50), "START GAME")) {
+		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2,100,50), "START GAME")) {
 			Application.LoadLevel(1);
+		}
+		if(GUI.Button(new Rect(Screen.width/2,Screen.height/1.65f,100,50), "OPTIONS")) {
+			//Application.LoadLevel(1);
+		}
+		if(GUI.Button(new Rect(Screen.width/2,Screen.height/1.40f,100,50), "EXIT GAME")) {
+			Application.Quit();
 		}
 	}
 }
