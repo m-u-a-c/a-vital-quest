d9343e64f97c7c49222d228c8a842d5b1616ba66
MightyKoala <leonpayao@gmail.com>
2014-11-20 09:14:27 +0100

    start

---
+++ New Unity Project/Assets/Menus/OverlayUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlayUI : MonoBehaviour
+{
+	public Texture btnTexture;
+	void OnGUI ()
+	{
+
+		if(GUI.Button(new Rect(Screen.width/2,Screen.height/1.40f,100,50), ""))
+		{
+
+		}
+	}
+}
