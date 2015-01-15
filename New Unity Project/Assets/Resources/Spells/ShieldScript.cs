63107fa50921c03422718adcdd1c66f8b1865371
ViggoL <viggo.lunden@gmail.com>
2015-01-15 16:42:08 +0100

    Added Yao's Shield
    + Yao's Shield (without sprite)

---
+++ New Unity Project/Assets/Resources/Spells/ShieldScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldScript : MonoBehaviour {
+
+    
+    public float destroytime = 3;
+    float timeleft;
+    // Use this for initialization
+    void Start()
+    {
+        timeleft = destroytime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+
+        timeleft -= Time.deltaTime;
+        if (timeleft <= 0)
+        {
+            Destroy(gameObject);
+            timeleft = destroytime;
+        }
+    }
+}
--- New Unity Project/Assets/char/Pinventory.cs
+++ New Unity Project/Assets/char/Pinventory.cs
@@ -3,63 +3,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class Pinventory : MonoBehaviour {
-	public string name;
-	public Sprite Chargebolt;
-	public Sprite Peashooter;
-	void Update()
-	{
-		spell.UpdateStats ();
-		name = spell.SpellName;
-//		var go = GameObject.Find ("UI_Inventory");
-//		go.guiText.text = "";
-//		if (items.Count == 0)
-//						go.guiText.text = "No items";
-//				else {
-//		foreach (BaseItem item in items)
-//				go.guiText.text += "\n" + item.ItemName;
-//		}
-//		go = GameObject.Find ("UI_Stats");
-//		go.guiText.text = "";
-//		go.guiText.text = 
-//				"Attack damage: " + gameObject.GetComponent<Pstats>().aDamage.ToString () + 
-//				"\nSpell damage: " + gameObject.GetComponent<Pstats>().sDamage.ToString () + 
-//				"\nAttack speed: " + gameObject.GetComponent<Pstats>().aSpeed.ToString () + 
-//				"%\nHealth: " + gameObject.GetComponent<Pstats>().health.ToString () + 
-//				"%\nMaxHealth: " + gameObject.GetComponent<Pstats>().maxhealth.ToString () + 
-//				"\nMovement speed: " + gameObject.GetComponent<Pstats>().movement + 
-//				"%\nCharges: " + gameObject.GetComponent<Pstats>().charges.ToString();
+public class Pinventory : MonoBehaviour
+{
+    public string name;
+    public Sprite Chargebolt;
+    public Sprite Peashooter;
+    public Sprite Shield;
+    void Update()
+    {
+        spell.UpdateStats();
+        name = spell.SpellName;
+        //		var go = GameObject.Find ("UI_Inventory");
+        //		go.guiText.text = "";
+        //		if (items.Count == 0)
+        //						go.guiText.text = "No items";
+        //				else {
+        //		foreach (BaseItem item in items)
+        //				go.guiText.text += "\n" + item.ItemName;
+        //		}
+        //		go = GameObject.Find ("UI_Stats");
+        //		go.guiText.text = "";
+        //		go.guiText.text = 
+        //				"Attack damage: " + gameObject.GetComponent<Pstats>().aDamage.ToString () + 
+        //				"\nSpell damage: " + gameObject.GetComponent<Pstats>().sDamage.ToString () + 
+        //				"\nAttack speed: " + gameObject.GetComponent<Pstats>().aSpeed.ToString () + 
+        //				"%\nHealth: " + gameObject.GetComponent<Pstats>().health.ToString () + 
+        //				"%\nMaxHealth: " + gameObject.GetComponent<Pstats>().maxhealth.ToString () + 
+        //				"\nMovement speed: " + gameObject.GetComponent<Pstats>().movement + 
+        //				"%\nCharges: " + gameObject.GetComponent<Pstats>().charges.ToString();
 
-	}
+    }
 
-	public List<BaseItem> items;
-	public BaseSpell spell;
+    public List<BaseItem> items;
+    public BaseSpell spell;
 
-	void Start () {
-		items = new List<BaseItem>();
-	}
-	public void AddItem (BaseItem item)
-	{
-		if (items.Count == 4) {
-		    items [3] = item;
-			return;
-	    }	
-		items.Add (item);
-		item.Effect ();
-		item.Stats ();
-	}
-	public void RemoveItem (BaseItem item)
-	{
-				item.RevertStats ();
-				items.Remove (item);
-	}
-	public void SetSpell (BaseSpell s)
-	{
-		spell = s;
-		if (s.SpellName == "Magic Peashooter")
-						GameObject.Find ("Spell").GetComponent<Image> ().sprite = Peashooter;
-				else
-						GameObject.Find ("Spell").GetComponent<Image> ().sprite = Chargebolt;
-	}
+    void Start()
+    {
+        items = new List<BaseItem>();
+    }
+    public void AddItem(BaseItem item)
+    {
+        if (items.Count == 4)
+        {
+            items[3] = item;
+            return;
+        }
+        items.Add(item);
+        item.Effect();
+        item.Stats();
+    }
+    public void RemoveItem(BaseItem item)
+    {
+        item.RevertStats();
+        items.Remove(item);
+    }
+    public void SetSpell(BaseSpell s)
+    {
+        spell = s;
+        Sprite sprite = null;
+        switch (s.SpellName)
+        {
+            case "Magic Peashooter":
+                sprite = Peashooter;
+                break;
+            case "Chargebolt":
+                sprite = Chargebolt;
+                break;
+            case "Yao's Shield":
+                sprite = Shield;
+                break;
+        }
+        GameObject.Find("Spell").GetComponent<Image>().sprite = sprite;
+    }
 
 }
